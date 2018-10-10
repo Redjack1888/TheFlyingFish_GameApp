119d8fca47e89e48b0f90a7ae4c6e95118078181
Alessandro Mana <alessandro.mana@gmail.com>
2018-10-10 23:19:55 +0200

    Fish Vertical Movement
    The FlyingFish is made by 2 images. To realize the movement on vertical axis we have to change and add some code to be able to "Animate" the fish when it moves.
    
    In FlyingFishView java Class:
    - Modify fish Bitmap to fish[] to be able to use both fish images.
    - Add int variables for canvas size (canvasWidth, canvasHeight).
    - Add int variables fishX, fishY, fishSpeed, minFishY, maxFishY to create FlyingFish movement.
    - Add FlyingFish start position.
    - Add if statements to locate the fish on the canvas.
    - Override an onTouchEvent() on getAction (ACTION_DOWN) to move the FlyingFish.
    
    In MainActivity:
    - Add an Handler.
    - Add an Interval.
    - Add a Timer that schedule a new Timer Task according to the Interval.

--- app/src/main/java/com/example/alessandro/theflyingfish/FlyingFishView.java
+++ app/src/main/java/com/example/alessandro/theflyingfish/FlyingFishView.java
@@ -7,11 +7,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class FlyingFishView extends View {
 
-    private Bitmap fish;
+    private Bitmap fish[] = new Bitmap[2] ;
+    private int fishX = 10;
+    private int fishY;
+    private int fishSpeed;
+
+    private int canvasWidth;
+    private int canvasHeight;
+
+    private boolean touch = false;
+
     private Bitmap backgroundImg;
     private Paint scorePaint = new Paint();
     private Bitmap life[] = new Bitmap[2];
@@ -20,7 +30,8 @@ public class FlyingFishView extends View {
 
         super(context);
         // Fish Image
-        fish = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
+        fish[0] = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
+        fish[1] = BitmapFactory.decodeResource(getResources(),R.drawable.fish2);
 
         // Background Image
         backgroundImg = BitmapFactory.decodeResource(getResources(),R.drawable.background);
@@ -35,6 +46,9 @@ public class FlyingFishView extends View {
         life[0] = BitmapFactory.decodeResource(getResources(),R.drawable.hearts);
         life[1] = BitmapFactory.decodeResource(getResources(),R.drawable.heart_grey);
 
+        // Fish Position on Start
+        fishY = 550;
+
     }
 
     @Override
@@ -42,15 +56,59 @@ public class FlyingFishView extends View {
 
         super.onDraw(canvas);
 
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
         canvas.drawBitmap(backgroundImg,0,0,null);
 
-        canvas.drawBitmap(fish,0,0,null);
+        int minFishY = fish[0].getHeight();
+        int maxFishY = canvasHeight - fish[0].getHeight() * 3;
+        fishY = fishY + fishSpeed;
+
+        if (fishY < minFishY){
+
+            fishY = minFishY;
+
+        }
+
+        if (fishY > maxFishY){
+
+            fishY = maxFishY;
+
+        }
+
+        fishSpeed = fishSpeed + 2;
+
+        if (touch){
+
+            canvas.drawBitmap(fish[1], fishX, fishY, null);
+            touch = false;
+
+        }else{
+
+            canvas.drawBitmap(fish[0], fishX, fishY,null);
+
+        }
 
         canvas.drawText(getContext().getString(R.string.score_label), 20,60,scorePaint);
 
         canvas.drawBitmap(life[0],580, 10,null);
         canvas.drawBitmap(life[0],680, 10,null);
         canvas.drawBitmap(life[1],780, 10,null);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+            touch = true;
+            fishSpeed = -22;
+
+        }
+
+        return true;
 
     }
 }
--- app/src/main/java/com/example/alessandro/theflyingfish/MainActivity.java
+++ app/src/main/java/com/example/alessandro/theflyingfish/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.alessandro.theflyingfish;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private FlyingFishView gameView;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
 
         gameView = new FlyingFishView(this);
         setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        gameView.invalidate();
+
+                    }
+                });
+
+            }
+        }, 0, Interval);
     }
 }
