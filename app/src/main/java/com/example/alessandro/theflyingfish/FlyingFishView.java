61f3dc53c2310694bc3c83f379cf884440c96e76
Alessandro Mana <alessandro.mana@gmail.com>
2018-10-10 21:27:35 +0200

    Set Game Objects
    Add to FlyingFishView class some elements of the game as background, score and game lifes. All elements will be displayed in the MainActivity as gameView.
    
    - Add to FlyingFishView java Class background Bitmap Image.
    - Add to FlyingFishView java Class life Bitmap Images as Array.
    - Add to FlyingFishView java Class Score Paint text.

--- app/src/main/java/com/example/alessandro/theflyingfish/FlyingFishView.java
+++ app/src/main/java/com/example/alessandro/theflyingfish/FlyingFishView.java
@@ -4,16 +4,37 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class FlyingFishView extends View {
 
     private Bitmap fish;
+    private Bitmap backgroundImg;
+    private Paint scorePaint = new Paint();
+    private Bitmap life[] = new Bitmap[2];
 
     public FlyingFishView(Context context) {
 
         super(context);
+        // Fish Image
         fish = BitmapFactory.decodeResource(getResources(),R.drawable.fish1);
+
+        // Background Image
+        backgroundImg = BitmapFactory.decodeResource(getResources(),R.drawable.background);
+
+        // Score Text
+        scorePaint.setColor(Color.WHITE);
+        scorePaint.setTextSize(70);
+        scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
+        scorePaint.setAntiAlias(true);
+
+        // Life Images
+        life[0] = BitmapFactory.decodeResource(getResources(),R.drawable.hearts);
+        life[1] = BitmapFactory.decodeResource(getResources(),R.drawable.heart_grey);
+
     }
 
     @Override
@@ -21,6 +42,15 @@ public class FlyingFishView extends View {
 
         super.onDraw(canvas);
 
+        canvas.drawBitmap(backgroundImg,0,0,null);
+
         canvas.drawBitmap(fish,0,0,null);
+
+        canvas.drawText(getContext().getString(R.string.score_label), 20,60,scorePaint);
+
+        canvas.drawBitmap(life[0],580, 10,null);
+        canvas.drawBitmap(life[0],680, 10,null);
+        canvas.drawBitmap(life[1],780, 10,null);
+
     }
 }
