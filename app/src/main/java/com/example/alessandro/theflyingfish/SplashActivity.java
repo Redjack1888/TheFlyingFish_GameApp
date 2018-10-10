453ac731b51b1f474b56113e5d4e8a48db6acbb8
Alessandro Mana <alessandro.mana@gmail.com>
2018-10-10 20:01:02 +0200

    Setting Splash to MainActivity Tempo
    - Modify Manifest.xml to make SplashActivity Launchable.
    - Add a Thread to SplashActivity so that after 5 seconds an intent is able to start and open MainActivity.

--- app/src/main/java/com/example/alessandro/theflyingfish/SplashActivity.java
+++ app/src/main/java/com/example/alessandro/theflyingfish/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.alessandro.theflyingfish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,37 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+
+                    sleep(5000);
+
+                }
+                catch (Exception e){
+
+                    e.printStackTrace();
+
+                }
+                finally{
+
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
+
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+        
     }
 }
